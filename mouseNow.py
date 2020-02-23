a8503a8796eed5a35d64d7b41e125849d55cd365
mappy_HISAO <m11232mh@gmail.com>
2020-02-23 12:32:23 +0900

    実行形式を改良

--- mouseNow.py
+++ mouseNow.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import pyautogui
 print("中断するにはCtrーCを押してください")
 
@@ -6,7 +7,7 @@ try:
         #マウス座標を取得
         x,y = pyautogui.position()
         position_str = 'X: ' + str(x).rjust(4) + 'Y: ' + str(y).rjust(4)
-        print(position_str, end='')
+        print(position_str , end='')
         print('\b' * len(position_str), end='', flush=True)
 
 except KeyboardInterrupt:
